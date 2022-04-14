46fdab0a5435b61147554ed6119cf0108c5d85f9
UNICON3 <andyoung1226@gmail.com>
2022-04-14 11:43:09 +0900

    upppp

--- start_core.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/unicon3/.bashrc
-source /opt/ros/melodic/setup.bash
-roscore
-
-exit 0
--- start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /home/unicon3/.bashrc
-source /opt/ros/melodic/setup.bash
-source /home/unicon3/catkin_ws/src/django_test/web_env/bin/activate
-python3 /home/unicon3/catkin_ws/src/django_test/web_test/manage.py runserver
-
-exit 0
