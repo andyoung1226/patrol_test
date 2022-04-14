7fcd52c6e3f61d9132d787bb415c1f1ed6d58c51
root <andyoung1226@gmail.com>
2022-04-14 10:53:18 +0900

    up

---
+++ start_core.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/unicon3/.bashrc
+source /opt/ros/melodic/setup.bash
+roscore
+
+exit 0
--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /home/unicon3/.bashrc
+source /opt/ros/melodic/setup.bash
 source /home/unicon3/catkin_ws/src/django_test/web_env/bin/activate
 python3 /home/unicon3/catkin_ws/src/django_test/web_test/manage.py runserver
 
